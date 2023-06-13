6ebc0665afa57dae7e735a2659a84bb05d701105
tram888 <bigtram888@gmail.com>
2023-06-13 09:52:52 +0900

    echobot for VK.

--- vk_bot.py
+++ vk_bot.py
@@ -1,6 +1,16 @@
-import vk_api
+import random
+
+import vk_api as vk
 from environs import Env
 from vk_api.longpoll import VkLongPoll, VkEventType
+
+
+def echo(event, vk_api):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.randint(1, 1000)
+    )
 
 
 def main():
@@ -8,18 +18,14 @@ def main():
     env.read_env()
     vk_group_token = env.str('VK_API_KEY')
 
-    vk_session = vk_api.VkApi(token=vk_group_token)
-
+    vk_session = vk.VkApi(token=vk_group_token)
+    vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
-
     for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            print('Новое сообщение:')
-            if event.to_me:
-                print('Для меня от: ', event.user_id)
-            else:
-                print('От меня для: ', event.user_id)
-            print('Текст:', event.text)
+        if event.type == VkEventType.MESSAGE_NEW and event.from_chat and event.to_me:
+            chat_id = event.chat_id
+            vk_api.messages.send(chat_id=chat_id, message=event.text, random_id=random.randint(1, 1000))
+            echo(event, vk_api)
 
 
 if __name__ == '__main__':
