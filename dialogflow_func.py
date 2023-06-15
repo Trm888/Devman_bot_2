6c8120250b2e22f1fc11b6250995f167ece42b94
tram888 <bigtram888@gmail.com>
2023-06-15 22:33:34 +0900

    Edit detect_intent function

--- dialogflow_func.py
+++ dialogflow_func.py
@@ -1,15 +1,14 @@
 from google.cloud import dialogflow
 
 
-def detect_intent(project_id, session_id, texts, language_code='ru-RU'):
+def detect_intent(project_id, session_id, text, language_code='ru-RU'):
     session_client = dialogflow.SessionsClient()
 
     session = session_client.session_path(project_id, session_id)
 
-    for text in texts:
-        text_input = dialogflow.TextInput(text=text, language_code=language_code)
-        query_input = dialogflow.QueryInput(text=text_input)
-        response = session_client.detect_intent(
-            request={"session": session, "query_input": query_input}
-        )
-        return response.query_result
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+    return response.query_result
